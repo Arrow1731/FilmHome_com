501c012acbf29e3d7afcdf6fd84a5dd40a3afc93
Sobirjon <sobirjonotajonov11@gmail.com>
2025-07-25 15:37:02 +0500

    swiper header

--- src/Pages/Movies_Shows.jsx
+++ src/Pages/Movies_Shows.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
+
+
+import MySwiper from "../Components/MySwiper";
+
 export default class Movie extends Component {
   render() {
     return (
-      <div>Movie</div>
+      <div>
+        <div className='container'>
+        <header>
+        <MySwiper />
+      </header>
+      </div>
+      </div>
     )
   }
 }
