69106253ef71b8ff46efdcbd631f374bfcd2d918
azizbek1701 <munisakh@gmail.com>
2025-07-25 14:05:45 +0500

    start project

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='text-center text-3xl font-bold'>Home Page</h1>
+      </div>
+    )
+  }
+}
